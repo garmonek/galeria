e3a742a758b9339c941baf887a1a922624c19de4
Garmonek <Garmonek@gmail.com>
2020-05-08 14:54:22 +0200

    Nice

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
 {
     /**
-     * @Route("/", name="welcome")
+     * @Route("/{name}", name="welcome", defaults={"name" = "Tanjiro"}, requirements={"name" = "[A-Za-z]+"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index($name)
     {
-        return $this->render('welcome/index.html.twig');
+        return $this->render('welcome/index.html.twig',
+            [
+                'name' => $name,
+            ]
+        );
     }
 }
