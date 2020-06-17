e9718d9e4519ce09dfc0bf8fbe795898c9456372
garmonek <garmonek@gmail.com>
2020-06-18 00:26:08 +0200

    yo2

--- src/Form/WallpaperType.php
+++ src/Form/WallpaperType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Image;
@@ -27,33 +28,32 @@ class WallpaperType extends AbstractType
      * This method is called for each type in the hierarchy starting from the
      * top most type. Type extensions can further modify the form.
      *
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder The form builder
+     * @param array $options The options
      * @see FormTypeExtensionInterface::buildForm()
      *
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder The form builder
-     * @param array                                        $options The options
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('file', FileType::class, [
-                    'mapped' => false,
-                    'label' => 'label_wallpaper',
-                    'required' => true,
-                    'constraints' => new Image(
-                        [
-                            'maxSize' => '8196k',
-                            'mimeTypes' => [
-                                'image/png',
-                                'image/jpeg',
-                                'image/pjpeg',
-                                'image/jpeg',
-                                'image/pjpeg',
-                            ],
-                            'mimeTypesMessage' => 'Unsupported Image File',
-                        ]
-                    ),
-                ]
-            )
+                'mapped' => false,
+                'label' => 'label_wallpaper',
+                'required' => true,
+                'constraints' => new Image(
+                    [
+                        'maxSize' => '8196k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/pjpeg',
+                            'image/jpeg',
+                            'image/pjpeg',
+                        ],
+                        'mimeTypesMessage' => 'Unsupported Image File',
+                    ]
+                ),
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'label' => 'label_category',
@@ -62,6 +62,10 @@ class WallpaperType extends AbstractType
                 'choice_label' => function ($category) {
                     return $category->getTitle();
                 }
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'label_description',
+                'required' => false,
             ]);
     }
 
